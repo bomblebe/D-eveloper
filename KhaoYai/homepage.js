c3576f108e2e2448189b4e61eebe0fcc5129328b
bomblebe <51816270+bomblebe@users.noreply.github.com>
2019-10-27 14:15:20 +0700

    ไฟล์ทั้งหมด

--- KhaoYai/homepage.js
+++ KhaoYai/homepage.js
@@ -7,4 +7,18 @@
       x[i].style.display = "none";  
     }
     document.getElementById(newstype).style.display = "block";  
+
+    var y = document.getElementsByClassName("newsbtn");
+    for (i = 0; i < y.length; i++) {
+      y[i].classList.remove("activeTab"); 
+    }
+    document.getElementsByClassName(newstype).classList.add("activeTab");
+    
   }
+
+  new Vue({
+    el:'.login',
+    data:{
+        seen: false
+    }
+});
